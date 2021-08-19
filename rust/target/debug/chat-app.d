688c5b5a1a553618d27a75e0039d7ce1d8a8f601
WarriorsSami <sbarbutdica@gmail.com>
2021-08-19 12:49:43 +0300

    created base folders

---
+++ rust/target/debug/chat-app.d
@@ -0,0 +1,1 @@
+/media/sami/Dev\ Space/chat-app-cli/rust/target/debug/chat-app: /media/sami/Dev\ Space/chat-app-cli/rust/src/main.rs
---
+++ rust/target/debug/deps/chat_app-ea8b1ea62dbc0023.d
@@ -0,0 +1,5 @@
+/media/sami/Dev Space/chat-app-cli/rust/target/debug/deps/chat_app-ea8b1ea62dbc0023: src/main.rs
+
+/media/sami/Dev Space/chat-app-cli/rust/target/debug/deps/chat_app-ea8b1ea62dbc0023.d: src/main.rs
+
+src/main.rs:
